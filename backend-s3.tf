d0d75323d545e2afc4a09ce382713fc1220b203c
subupandian09 <85981714+subupandian09@users.noreply.github.com>
2022-09-05 01:29:23 -0500

    Add files via upload

---
+++ backend-s3.tf
@@ -0,0 +1,12 @@
+
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "tf-state-bucket-1908-01"
+  }
+}
+
+
